19a0bb17a7335fbfecd090f6100512bccc2c2598
rinrinrin2002 <rin@rinrin.me>
2021-01-23 17:13:23 +0900

    update install.zsh

--- install.zsh
+++ install.zsh
@@ -13,17 +13,28 @@ done
 
 if [[ $1 == init ]]; then
   if ! type brew &>/dev/null; then
+    printf '\e[32m\n\n%s\n\n\e[m' '> Install Homebrew'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
-
-  if type brew &>/dev/null; then
     brew doctor && brew bundle --global
   fi
 
   if type gh &>/dev/null; then
-    gh repo clone rinrinrin2002/OpenCore-B75-Pro3-M
+    printf '\e[32m\n\n%s\n\n\e[m' '> Download Proprietary Software'
     gh auth login && \
       gh repo clone rinrinrin2002/dotfiles-private && \
         /bin/zsh dotfiles-private/download.zsh
+
+    printf '\e[32m\n\n%s\n\n\e[m' '> Download OpenCore'
+    gh repo clone rinrinrin2002/OpenCore-B75-Pro3-M && {
+        printf '\e[32m\n\n%s\n\n\e[m' '> Install OpenCore'
+        diskutil list internal physical
+        printf '\e[33m%s\e[m' 'Which device do you want to install OpenCore on(ex. "disk0")? '
+        read device
+        sudo diskutil mount /dev/"$device"s1 && {
+            cp -Rv OpenCore-B75-Pro3-M/EFI /Volumes/EFI
+            cp -Rv OpenCore-B75-Pro3-M/Utilities /Volumes/EFI
+          }
+        diskutil unmount /dev/"$device"s1
+      }
   fi
 fi
