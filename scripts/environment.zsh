d49699d0a80783c42b7da0adef464f36163f5aed
rinrinrin2002 <rin@rinrin.me>
2021-01-26 04:03:05 +0900

    update environment.zsh

--- scripts/environment.zsh
+++ scripts/environment.zsh
@@ -3,7 +3,7 @@
 printf "\n\n\033[32m%s\033[m\n" "> Set up the environment"
 
 export NVM_DIR="$HOME/.nvm"
-source "$(brew --prefix)/opt/nvm/nvm.sh"
+source "/usr/local/opt/nvm/nvm.sh"
 nvm install --lts
 
 echo
