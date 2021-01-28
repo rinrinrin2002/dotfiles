223e7b4da53aeb585d43cd58f4cef5ed5fd3aba8
rinrinrin4046 <rin@rinrin.me>
2021-01-29 08:30:40 +0900

    update git.zsh

--- scripts/git.zsh
+++ scripts/git.zsh
@@ -2,12 +2,12 @@
 
 printf "\n\n\033[32m%s\033[m\n" "> Configure Git"
 
-echo "pinentry-program /usr/local/bin/pinentry-mac" > "${HOME}/.gnupg/gpg-agent.conf"
-gpgconf --kill gpg-agent
-
 keybase login
 keybase pgp export | gpg --import
 keybase pgp export -s | gpg --import
+
+echo "pinentry-program /usr/local/bin/pinentry-mac" > "${HOME}/.gnupg/gpg-agent.conf"
+gpgconf --kill gpg-agent
 
 echo
 
