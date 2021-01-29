fbf53957ac78739eca2ff2307004db6803e9040e
rinrinrin4046 <rin@rinrin.me>
2021-01-30 00:18:37 +0900

    update opencore.zsh

--- scripts/opencore.zsh
+++ scripts/opencore.zsh
@@ -8,13 +8,13 @@ read install
 if [[ ${(L)install:-n} == y ]]; then
   printf "\n\033[32m%s\033[m\n" "> Install OpenCore"
   git clone https://github.com/rinrinrin2002/OpenCore-B75-Pro3-M ${OPENCORE}
-  echo 
+  echo
   diskutil list internal physical
   printf "\033[33m%s\033[m" "Device name (ex. \"disk0\"): "
   read device
 
   sudo diskutil mount "/dev/${device}s1" && {
-    rsync -av --delete --exclude "/.*" --exclude shellx64.efi "${OPENCORE}/" /Volumes/EFI
+    rsync -av --delete --exclude ".*" --exclude shellx64.efi "${OPENCORE}/" /Volumes/EFI
     diskutil unmount "/dev/${device}s1"
   }
 fi
