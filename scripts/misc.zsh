559008175a6601f61c6b7b86022ae1f65166597b
rinrinrin4046 <rin@rinrin.me>
2021-03-22 02:01:37 +0900

    update misc.zsh

--- scripts/misc.zsh
+++ scripts/misc.zsh
@@ -2,12 +2,8 @@
 
 printf "\n\n\033[32m%s\033[m\n" "> Miscellaneous"
 
-: "Wallpaper and workflows" && () {
+: "Hard link to the wallpaper" && () {
   ln -nfv "${DOTFILES}/wallpaper.png" "${HOME}/Pictures/wallpaper.png"
-  rsync -a --delete "${DOTFILES}/workflows" "${DOTFILES}/tmp"
-  for f in ${DOTFILES}/tmp/workflows/*; do
-    open -W ${f}
-  done
 }
 
 echo
