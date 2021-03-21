c39139a44f35179234b1176d3c13c4f1ea03bb0d
rinrinrin4046 <rin@rinrin.me>
2021-03-22 02:03:49 +0900

    update bundle.zsh

--- scripts/bundle.zsh
+++ scripts/bundle.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-printf "\n\n\033[32m%s\033[m\n" "> Install software from the Brewfile"
+printf "\n\n\033[32m%s\033[m\n" "> Install software"
 
 brew doctor
 sudo softwareupdate --install-rosetta
