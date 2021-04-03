e0530ba7c1fb0b280f0f7b492d56ba93d11f4dc5
rinrinrin4046 <72665456+rinrinrin4046@users.noreply.github.com>
2021-04-04 01:49:48 +0900

    Update misc.zsh

--- scripts/misc.zsh
+++ scripts/misc.zsh
@@ -4,6 +4,10 @@ DOTFILES="${HOME}/.dotfiles"
 
 printf "\n\n\033[32m%s\033[m\n" "> Miscellaneous"
 
+: "Sign in to GitHub" && () {
+  gh auth login
+}
+
 : "Install Node.js" && () {
   source "/opt/homebrew/opt/nvm/nvm.sh"
   nvm install node
